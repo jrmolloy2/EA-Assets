71d86ba0979fa75963c3de6fcf4f0ebbc7e4a7e4
Julia Molloy <jrmolloy93@outlook.com>
2023-01-16 06:48:13 +0000

    AssetData class and tests

--- asset_checker/assets.py
+++ asset_checker/assets.py
@@ -3,7 +3,7 @@ from asset_checker.errors import InvalidAPICallError
 
 
 ROOT = "http://environment.data.gov.uk/asset-management"
-SINGLE_ASSET = ROOT + "/id/asset/{}.json"
+SINGLE_ASSET = ROOT + "/mapping-api/Asset?assets=http://environment.data.gov.uk/asset-management/id/asset/{}"
 
 
 class CallAsset:
@@ -18,3 +18,37 @@ class CallAsset:
             return response.json()
         else:
             raise InvalidAPICallError(response.status_code)
+
+
+class AssetData:
+
+    def __init__(self, data):
+        self.data = data
+        self._asset_type = None
+        self._asset_subtype = None
+        self._geom_type = None
+
+    def set_asset_type(self):
+        if self.data:
+            try:
+                self._asset_type = self.data[0]["asset-type"]["label"]
+                self._asset_subtype = self.data[0]["asset-sub-type"]["label"]
+            except KeyError:
+                raise KeyError("Unexpected format of JSON response from the API.")
+        else:
+            raise IndexError("No asset data returned from the API.")
+
+    def get_asset_type(self):
+        return self._asset_type + ": " + self._asset_subtype
+
+    def set_geometry_type(self):
+        if self.data:
+            try:
+                self._geom_type = self.data[0]["geometry"]["type"]
+            except KeyError:
+                raise KeyError("Unexpected format of JSON response from the API.")
+        else:
+            raise IndexError("No asset data returned from the API.")
+
+    def get_geometry_type(self):
+        return self._geom_type
--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest import mock
-from asset_checker.assets import CallAsset
+from asset_checker.assets import CallAsset, AssetData
 from asset_checker.errors import InvalidAPICallError
 
 
@@ -27,6 +27,94 @@ class TestCallAsset(unittest.TestCase):
         asset_call = CallAsset(263188)
         with self.assertRaises(InvalidAPICallError):
             asset_call.get_asset_information()
+
+
+class TestAssetData(unittest.TestCase):
+
+    def test_init_valid(self):
+        asset_data = AssetData("test")
+        self.assertEqual("test", asset_data.data)
+
+    def test_set_asset_type_valid(self):
+        mock_json = [
+            {
+                "id": "mock_url",
+                "type": "mock_url",
+                "label": "mock_asset:id",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-0.1952, 53.08209]
+                },
+                "asset-type": {
+                    "id": "mock_url",
+                    "label": "Structure"
+                },
+                "asset-sub-type": {
+                    "id": "mock_url",
+                    "label": "Outfall"
+                }
+            }
+        ]
+        asset_data = AssetData(mock_json)
+        asset_data.set_asset_type()
+        self.assertEqual("Structure: Outfall", asset_data.get_asset_type())
+
+    def test_get_asset_type_invalid_no_data(self):
+        mock_json = []
+        asset_data = AssetData(mock_json)
+        with self.assertRaises(IndexError):
+            asset_data.set_asset_type()
+
+    def test_get_asset_type_invalid_unexpected_structure(self):
+        mock_json = [
+            {
+                "id": "mock_url"
+            }
+        ]
+        asset_data = AssetData(mock_json)
+        with self.assertRaises(KeyError):
+            asset_data.set_asset_type()
+
+    def test_set_geometry_type_valid(self):
+        mock_json = [
+            {
+                "id": "mock_url",
+                "type": "mock_url",
+                "label": "mock_asset:id",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-0.1952, 53.08209]
+                },
+                "asset-type": {
+                    "id": "mock_url",
+                    "label": "Structure"
+                },
+                "asset-sub-type": {
+                    "id": "mock_url",
+                    "label": "Outfall"
+                }
+            }
+        ]
+        asset_data = AssetData(mock_json)
+        asset_data.set_geometry_type()
+        
+        self.assertEqual("Point", asset_data.get_geometry_type())
+
+    def test_set_geometry_type_invalid_no_data(self):
+        mock_json = []
+        asset_data = AssetData(mock_json)
+
+        with self.assertRaises(IndexError):
+            asset_data.set_geometry_type()
+
+    def test_set_geometry_type_invalid_unexpected_structure(self):
+        mock_json = [
+            {"id": "mock_url"}
+        ]
+        asset_data = AssetData(mock_json)
+
+        with self.assertRaises(KeyError):
+            asset_data.set_geometry_type()
 
 
 if __name__ == "__main__":
